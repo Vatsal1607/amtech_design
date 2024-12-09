4591453d40bf36831fe9b46e21c19e76b0801ca9
Vatsal Nayi <silentkiller8128@gmail.com>
2024-12-09 23:30:52 +0530

    api call of business list

--- lib/services/network/api/api_constants.dart
+++ lib/services/network/api/api_constants.dart
@@ -1,3 +1,4 @@
 class BaseUrl {
-  static const String apiBaseUrl = 'http://192.168.1.8:9000/';
+  // static const String apiBaseUrl = 'http://192.168.1.8:9000/';
+  static const String apiBaseUrl = 'https://1bef-2409-40c1-32-7d15-2e39-b56b-7e96-39a6.ngrok-free.app/';
 }
